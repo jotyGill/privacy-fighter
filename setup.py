3bb0e0e73c53ed6f2179e436b529e6e5898358e2
JGill <joty@mygnu.org>
2019-02-21 13:42:19 +1030

    Setup

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 import sys
 
 import setuptools
+# import os
 
 # from pillowcover_gui import __version__
 
-__version__ = "0.0.3"
+# pf_location = os.path.join(os.path.abspath(os.path.dirname(__file__)), "privacyfighter")
+# exec(open(os.path.join(pf_location, "version.py")).read())
+__version__ = "0.0.9"
 
 if sys.version_info < (3, 5):
     sys.stderr.write(
@@ -25,29 +28,38 @@ setuptools.setup(
     author="JGill",
     zip_safe=False,
     author_email="joty@mygnu.org",
-    url="https://github.com/jotyGill/pillowcover",
-    keywords=["images", "image-minipulation"],
+    url="https://github.com/jotyGill/",
+    keywords=["privacy", "firefox", "browser"],
     python_requires='>=3.5',
-    install_requires=[],
+    install_requires=['requests'],
     platforms=["GNU/Linux", "Ubuntu", "Debian", "Kali", "CentOS", "Arch", "Fedora"],
     packages=setuptools.find_packages(),
     entry_points={
         "console_scripts": [
             "pf = privacyfighter.pf:main",
+            "privacyfighter = privacyfighter.pf:main",
         ]
     },
+    include_package_data=True,
     long_description=full_description,
     long_description_content_type="text/markdown",
     classifiers=[
         "Intended Audience :: End Users/Desktop",
         "Intended Audience :: System Administrators",
+        "Intended Audience :: Information Technology",
         "Topic :: Utilities",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Natural Language :: English",
+        "Operating System :: OS Independent",
         "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft",
+        "Operating System :: MacOS",
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Topic :: Internet",
+        "Topic :: Internet :: WWW/HTTP :: Browsers",
+        "Topic :: Security",
     ],
 )
