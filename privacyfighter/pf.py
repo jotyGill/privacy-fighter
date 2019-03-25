85addc657d8256d75c9714139d174ec51694d361
JGill <joty@mygnu.org>
2019-03-25 13:30:59 +1030

    Move to using "user-overrides.js"

--- privacyfighter/pf.py
+++ privacyfighter/pf.py
@@ -15,7 +15,7 @@ import requests
 import psutil
 from gooey import Gooey, GooeyParser
 
-__version__ = "0.0.10"
+__version__ = "0.0.11"
 __basefilepath__ = os.path.dirname(os.path.abspath(__file__))
 
 # temporary folder to download files in
@@ -32,7 +32,6 @@ os.makedirs(extensions_folder, exist_ok=True)
 pref_add = [
     # {'pref': '"browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts.searchEngines"',
     #  'value': '"duckduckgo"'},
-    {'pref': '"app.update.auto"', 'value': 'true'},     # enable auto updates
     {'pref': '"privacyfighter.version"', 'value': '{}'.format(
         __version__)},     # Privacy Fighter Version
     # remove auto included Google, Youtube and Facebook shortcuts on newtabpage
@@ -45,186 +44,6 @@ pref_add = [
     {'pref': '"privacy.window.maxInnerHeight"', 'value': '900'},
 ]
 
-# preferences to be modified in the users.j. if 'value' is given in here, it will be overwritten
-# using these. If 'value' is '', that preference will be deleted from users.js , so Firefox's Default
-# or user's original preference stays in place.
-# '' is used to get rid of undesired preferences set in ghacks's user.js. such as "places.history.enabled", "false"
-pref_mods = [
-    # SECTION 0100: STARTUP
-    # dont enforce not checking defalut browser
-    {'pref': '"browser.shell.checkDefaultBrowser"', 'value': ''},
-    {'pref': '"browser.startup.homepage"', 'value': ''},
-    {'pref': '"browser.startup.page"', 'value': ''},
-    {'pref': '"browser.newtabpage.enabled"', 'value': ''},
-
-
-    # [SECTION 0200]: GEOLOCATION
-    # [SECTION 0300]: QUIET FOX
-    {'pref': '"app.update.auto"', 'value': ''},
-    {'pref': '"extensions.update.autoUpdateDefault"', 'value': 'true'},
-    {'pref': '"app.update.service.enabled"', 'value': ''},
-    {'pref': '"app.update.silent"', 'value': ''},
-    {'pref': '"app.update.staging.enabled"', 'value': ''},
-    {'pref': '"browser.search.update"', 'value': ''},
-
-    {'pref': '"extensions.systemAddon.update.enabled"', 'value': ''},
-    {'pref': '"extensions.systemAddon.update.url"', 'value': ''},
-    {'pref': '"network.dns.disableIPv6"', 'value': ''},
-    {'pref': '"network.http.spdy.enabled"', 'value': ''},
-    {'pref': '"network.http.spdy.enabled.deps"', 'value': ''},
-    {'pref': '"network.http.spdy.enabled.http2"', 'value': ''},
-    {'pref': '"network.http.spdy.websockets"', 'value': ''},
-
-    {'pref': '"network.http.altsvc.enabled"', 'value': ''},
-    {'pref': '"network.http.altsvc.oe"', 'value': ''},
-    {'pref': '"network.file.disable_unc_paths"', 'value': ''},
-    {'pref': '"captivedetect.canonicalURL"', 'value': ''},
-    {'pref': '"network.captive-portal-service.enabled"', 'value': ''},
-    {'pref': '"network.gio.supported-protocols"', 'value': ''},
-    # [SECTION 0500]: SYSTEM ADD-ONS / EXPERIMENTS
-    {'pref': '"extensions.formautofill.addresses.enabled"', 'value': ''},
-    {'pref': '"extensions.formautofill.available"', 'value': ''},
-    {'pref': '"extensions.formautofill.heuristics.enabled"', 'value': ''},
-
-    # [SECTION 0800]: LOCATION BAR / SEARCH BAR / SUGGESTIONS / HISTORY / FORMS
-
-
-    {'pref': '"keyword.enabled"', 'value': ''},         # don't block search from urlbar
-    # {'pref': '"browser.search.suggest.enabled"', 'value': ''},    # live searches in urlbar
-    # {'pref': '"browser.urlbar.suggest.searches"', 'value': ''},
-    {'pref': '"network.file.disable_unc_paths"', 'value': ''},
-    {'pref': '"browser.formfill.enable"', 'value': ''},
-
-    # [SECTION 0900]: PASSWORDS
-
-    {'pref': '"security.ask_for_password"', 'value': ''},
-    {'pref': '"security.password_lifetime"', 'value': ''},
-    {'pref': '"signon.autofillForms"', 'value': ''},     # allow over tls but not for http
-    {'pref': '"signon.autofillForms.http"', 'value': 'false'},  # DEL
-    {'pref': '"network.auth.subresource-http-auth-allow"', 'value': ''},
-    {'pref': '"signon.formlessCapture.enabled"', 'value': ''},
-
-    # [SECTION 1000]: CACHE / SESSION (RE)STORE / FAVICONS
-    {'pref': '"browser.sessionstore.interval"', 'value': ''},
-
-    # [SECTION 1200]: HTTPS (SSL/TLS / OCSP / CERTS / HPKP / CIPHERS)
-    {'pref': '"security.ssl.require_safe_negotiation"', 'value': ''},
-    {'pref': '"security.tls.enable_0rtt_data"', 'value': ''},
-
-    {'pref': '"security.OCSP.require"', 'value': ''},
-    {'pref': '"security.pki.sha1_enforcement_level"', 'value': ''},     # dont disable SHA-1 certificates
-    # dont disable windows family safety cert
-    {'pref': '"security.family_safety.mode"', 'value': ''},
-    {'pref': '"security.cert_pinning.enforcement_level"', 'value': ''},  # dont inforce cert pinning
-    {'pref': '"security.mixed_content.block_display_content"', 'value': ''},
-
-    # [SECTION 1400]: FONTS
-    # {'pref': '"browser.display.use_document_fonts"', 'value': ''},  #
-    {'pref': '"gfx.font_rendering.opentype_svg.enabled"', 'value': ''},
-    {'pref': '"gfx.downloadable_fonts.woff2.enabled"', 'value': ''},
-    # {'pref': '"layout.css.font-loading-api.enabled"', 'value': ''},
-    {'pref': '"gfx.font_rendering.graphite.enabled"', 'value': ''},
-
-    # [SECTION 1600]: HEADERS / REFERERS
-    {'pref': '"network.http.referer.XOriginPolicy"', 'value': ''},
-    # [SECTION 1700]: CONTAINERS
-    # [SECTION 1800]: PLUGINS
-    {'pref': '"plugin.default.state"', 'value': ''},
-    {'pref': '"plugin.defaultXpi.state"', 'value': ''},
-    {'pref': '"plugin.sessionPermissionNow.intervalInMinutes"', 'value': ''},
-
-    {'pref': '"plugin.state.flash"', 'value': ''},  # dont force disable flash
-    {'pref': '"plugin.scan.plid.all"', 'value': ''},  # dont disable flash
-    # dont disable all GMP (Gecko Media Plugins)
-    {'pref': '"media.gmp-provider.enabled"', 'value': ''},
-    {'pref': '"media.gmp.trial-create.enabled"', 'value': ''},
-    {'pref': '"media.gmp-manager.url"', 'value': ''},
-    {'pref': '"media.gmp-manager.url.override"', 'value': ''},
-    {'pref': '"media.gmp-manager.updateEnabled"', 'value': ''},
-
-    # dont disable widevine CDM (Content Decryption Module)
-    {'pref': '"media.gmp-widevinecdm.visible"', 'value': ''},
-    {'pref': '"media.gmp-widevinecdm.enabled"', 'value': ''},
-    {'pref': '"media.gmp-widevinecdm.autoupdate"', 'value': ''},
-
-    # dont disable all DRM content (EME: Encryption Media Extension)
-    {'pref': '"media.eme.enabled"', 'value': ''},
-    # dont disable the OpenH264 Video Codec by Cisco to
-    {'pref': '"media.gmp-gmpopenh264.enabled"', 'value': ''},
-    {'pref': '"media.gmp-gmpopenh264.autoupdate"', 'value': ''},
-    # [SECTION 2000]: MEDIA / CAMERA / MIC
-    # keep WebRTC but expose only default ip (don't leak)
-    {'pref': '"media.peerconnection.enabled"', 'value': ''},
-    {'pref': '"media.peerconnection.ice.default_address_only"', 'value': 'true'},
-    {'pref': '"media.peerconnection.ice.no_host"', 'value': 'true'},
-
-    # dont disable screensharing
-    {'pref': '"media.getusermedia.screensharing.enabled"', 'value': ''},
-    {'pref': '"media.getusermedia.browser.enabled"', 'value': ''},
-    {'pref': '"media.getusermedia.audiocapture.enabled"', 'value': ''},
-
-    {'pref': '"canvas.capturestream.enabled"', 'value': ''},
-    {'pref': '"media.autoplay.default"', 'value': ''},
-    # [SECTION 2200]: WINDOW MEDDLING & LEAKS / POPUPS
-    # [SECTION 2300]: WEB WORKERS
-    # dont disable web workers, webnotifications
-    {'pref': '"dom.serviceWorkers.enabled"', 'value': ''},
-    {'pref': '"dom.webnotifications.enabled"', 'value': ''},
-    {'pref': '"dom.webnotifications.serviceworker.enabled"', 'value': ''},
-    # dont disable push notifications
-    {'pref': '"dom.push.enabled"', 'value': ''},
-    {'pref': '"dom.push.connection.enabled"', 'value': ''},
-    {'pref': '"dom.push.serverURL"', 'value': ''},
-    {'pref': '"dom.push.userAgentID"', 'value': ''},
-    # [SECTION 2400]: DOM (DOCUMENT OBJECT MODEL) & JAVASCRIPT
-    {'pref': '"dom.event.clipboardevents.enabled"', 'value': ''},
-    # reconsider, disable "Confirm you want to leave" dialog on page close
-    {'pref': '"dom.disable_beforeunload"', 'value': ''},
-    {'pref': '"dom.allow_cut_copy"', 'value': ''},
-
-    {'pref': '"javascript.options.asmjs"', 'value': ''},    # dont disable asm.js
-    {'pref': '"javascript.options.wasm"', 'value': ''},    # dont disable WebAssembly
-    {'pref': '"dom.vibrator.enabled"', 'value': ''},
-    {'pref': '"dom.IntersectionObserver.enabled"', 'value': ''},
-
-    {'pref': '"javascript.options.shared_memory"', 'value': ''},
-
-    # [SECTION 2500]: HARDWARE FINGERPRINTING
-    # [SECTION 2600]: MISCELLANEOUS
-    {'pref': '"browser.tabs.remote.allowLinkedWebInFileUriProcess"', 'value': ''},
-    {'pref': '"browser.pagethumbnails.capturing_disabled"', 'value': ''},
-    {'pref': '"browser.uitour.enabled"', 'value': ''},
-    {'pref': '"browser.uitour.url"', 'value': ''},
-    {'pref': '"mathml.disabled"', 'value': ''},
-    {'pref': '"network.IDN_show_punycode"', 'value': ''},
-    {'pref': '"pdfjs.disabled"', 'value': ''},  # allow inbuilt pdfviewer
-
-    {'pref': '"network.protocol-handler.external.ms-windows-store"', 'value': ''},
-    {'pref': '"devtools.chrome.enabled"', 'value': ''},
-    {'pref': '"browser.download.manager.addToRecentDocs"', 'value': ''},
-    {'pref': '"browser.download.forbid_open_with"', 'value': ''},
-    {'pref': '"security.csp.experimentalEnabled"', 'value': ''},
-
-    # [SECTION 2700]: PERSISTENT STORAGE
-    # allow third party cookies, but session only and "4" exclude known trackers
-    {'pref': '"network.cookie.cookieBehavior"', 'value': '4'},
-
-    # don't enforce history and downloads to clear on shutdown
-    {'pref': '"privacy.sanitize.sanitizeOnShutdown"', 'value': 'true'},
-    {'pref': '"privacy.clearOnShutdown.history"', 'value': 'false'},
-    {'pref': '"privacy.clearOnShutdown.formdata"', 'value': 'false'},
-    {'pref': '"privacy.clearOnShutdown.openWindows"', 'value': 'false'},
-    {'pref': '"privacy.clearOnShutdown.downloads"', 'value': 'false'},
-    # // dont set time range to "Everything" as default in "Clear Recent History"
-    {'pref': '"privacy.sanitize.timeSpan"', 'value': ''},
-    {'pref': '"dom.caches.enabled"', 'value': ''},
-
-    # [SECTION 4000]: FPI (FIRST PARTY ISOLATION)
-    # disable first party isolation, we use temporary_containers
-    {'pref': '"privacy.firstparty.isolate"', 'value': 'false'},
-    # [SECTION 4500]: RFP (RESIST FINGERPRINTING)
-
-]
 
 extensions = [
     {'name': 'decentraleyes', 'id': 'jid1-BoFifL9Vbdl2zQ@jetpack.xpi',
@@ -321,11 +140,11 @@ def setup_userjs():
 
             sys.stdout.write(line)
 
-    # Append additional prefs to user.js from "pref_add"
+    # Append "user-overrides.js", to "user.js". this will add additional prefs
     with open(os.path.join(temp_folder, "user.js"), "a") as userjs:
-        for i in pref_add:
-            line = 'user_pref({}, {});\n'.format(i['pref'], i['value'])
-            userjs.write(line)
+        with open(os.path.join(resource_path("profile"), "user-overrides.js"), "r") as user_overrides:
+            for line in user_overrides:
+                userjs.write(line)
 
 
 def extract_user_overrides():
@@ -346,7 +165,7 @@ def extract_user_overrides():
                 remove_prefs.append(pref_comment_pair)
             elif line[:9] == "user_pref":
                 pref_comment_pair = []
-                pref_comment_pair.append(line.stip("\n"))
+                pref_comment_pair.append(line.strip("\n"))
     # print(remove_prefs)
     return(remove_prefs)
 
@@ -441,8 +260,8 @@ def run(profile_name):
         print("ERROR: 'Profile Name' string matches more than one profile folders, please provide a full name instead: ", profiles, "\n")
         sys.exit(1)
 
-    download_extensions()
     setup_userjs()
+    download_extensions()
 
     print("\nModified Preferences (Users.js) and Extensions will now be copied to {}\n".format(profile))
     # firefox profile path on the os
